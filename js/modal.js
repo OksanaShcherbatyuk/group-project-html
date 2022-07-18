52b5fd650d4e981f36bcdbd5b7082be59b038afa
OksanaShcherbatyuk <anbony@gmail.com>
2022-07-18 22:26:25 +0300

    робить дві модалки

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,24 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
+      openModalBtn1: document.querySelector('[data-modal-open-two]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
+      closeModalBtn1: document.querySelector('[data-modal-close-two]'),
       modal: document.querySelector('[data-modal]'),
+      modal1: document.querySelector('[data-modal-two]'),
     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal1);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn1.addEventListener('click', toggleModal1);
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
+      
+    }
+    function toggleModal1() {
+      
+      refs.modal1.classList.toggle('is-hidden1');
     }
   })();
